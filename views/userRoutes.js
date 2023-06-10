1fd0605b8658bd415337669234afa4a812f6522e
orianig <oriana.ig016@gmail.com>
2023-06-10 18:47:29 +0200

    refactor: code cleanup

--- views/userRoutes.js
+++ views/userRoutes.js
@@ -1,10 +1,11 @@
+const router = require('express').Router();
 const userController = require('../controllers/userController');
 const isStaff = require('../middlewares/isStaff');
 const auth = require('../middlewares/verifyToken');
-const router = require('express').Router();
 
 // router.get('/', auth, isSuperAdmin, userController.getAllUsers)
 router.get('/profile', auth, userController.getUserProfile)
-router.get('/allProfiles', auth,isStaff, userController.getAllUsersProfile)
+router.get('/allProfiles', auth, isStaff, userController.getAllUsersProfile)
 
 module.exports = router;
+
