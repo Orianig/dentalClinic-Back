13f7ab6faeacce1e9170aa8f836f1ce96d28e66a
orianig <oriana.ig016@gmail.com>
2023-06-10 18:46:00 +0200

    feat: add routes to appointments

--- router.js
+++ router.js
@@ -4,12 +4,13 @@ const router = require('express').Router();
 
 //importacion del modulo desde el directorio views y se asigna a una variable para su posterior llamada
 const authRoutes = require('./views/authRoutes');
-const userRoutes = require('./views/userRoutes')
+const userRoutes = require('./views/userRoutes');
+const appointRoutes = require('./views/appointRoutes')
 
 
 //accesibilidad a la ruta
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
-
+router.use('/appointments', appointRoutes);
 
 module.exports = router;
