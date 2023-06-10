0ee4844fe43077f2d632825fd056e49b89e8b85b
orianig <oriana.ig016@gmail.com>
2023-06-10 22:16:06 +0200

    feat: add update profile - user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const { User } = require('../models')
+const bcrypt = require('bcrypt');
 const userController = {}
 
 //Obtencion del perfil de cada usuario
@@ -66,5 +67,105 @@ userController.getAllUsersProfile = async (req, res) => {
         )
     }
 }
+
+//realizar el update de los usuarios
+userController.updateProfile = async (req, res) => {
+    try {
+        //si le meto el token erroneo no me a poder actualizar el profile
+        const userId = req.userId;
+        console.log(userId)
+        const user = await User.findByPk(userId);
+
+        if (!user) {
+            return res.json(
+                {
+                    success: true,
+                    message: "User doesnt exists"
+                }
+            );
+        };
+
+        const { name, lastName, email, password, dni, phoneNumber, gender, birthdate, specialityId, collegiateNumber } = req.body;
+
+        const updateFields = {};
+
+        if (name) {
+            updateFields.name = name;
+        }
+
+        if (lastName) {
+            updateFields.lastName = lastName;
+        }
+
+        if (email) {
+            updateFields.email = email;
+        }
+
+        if (password) {
+            if (password.length < 6) {
+                return res.send('Password must be longer than 6 characters');
+            }
+
+            const hasUppercase = /[A-Z]/.test(password);
+            const hasLowercase = /[a-z]/.test(password);
+            const hasNumber = /\d/.test(password);
+
+            if (!hasUppercase || !hasLowercase || !hasNumber) {
+                return res.send('Password must contain at least one uppercase letter, one lowercase letter, and one number');
+            }
+
+            const newPassword = bcrypt.hashSync(password, 8);
+            updateFields.password = newPassword;
+        }
+
+        if (dni) {
+            updateFields.dni = dni;
+        }
+
+        if (phoneNumber) {
+            updateFields.phoneNumber = phoneNumber;
+        }
+
+        if (gender) {
+            updateFields.gender = gender;
+        }
+
+        if (birthdate) {
+            updateFields.birthdate = birthdate;
+        }
+
+        if (specialityId) {
+            updateFields.specialityId = specialityId;
+        }
+
+        if (collegiateNumber) {
+            updateFields.collegiateNumber = collegiateNumber;
+        }
+
+        const userUpdated = await User.update(updateFields, {
+            where: {
+                id: userId
+            }
+        });
+        console.log(userUpdated)
+        return res.json(
+            {
+                success: true,
+                message: "User updated",
+                data: userUpdated
+            }
+        );
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: "User cant be updated",
+                error: error
+            }
+        )
+    }
+}
+
+
 
 module.exports = userController;
--- views/userRoutes.js
+++ views/userRoutes.js
@@ -6,6 +6,8 @@ const auth = require('../middlewares/verifyToken');
 // router.get('/', auth, isSuperAdmin, userController.getAllUsers)
 router.get('/profile', auth, userController.getUserProfile)
 router.get('/allProfiles', auth, isStaff, userController.getAllUsersProfile)
+// update de los perfiles de usuario
+router.put('/updateProfile', auth, userController.updateProfile)
 
 module.exports = router;
 
