9ebd06cd7ea2756c93069a43d46b45e457101bdd
orianig <oriana.ig016@gmail.com>
2023-06-12 12:03:32 +0200

    feat: add new create doctor - only admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,48 @@
 const { User } = require('../models')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const userController = {}
+
+//registro de un nuevo usuario en el sistema
+userController.register = async (req, res) => {
+    try {
+        const password = req.body.password;
+        if (password.length < 6) {
+            return res.send('Password must be longer than 6 characters');
+        }
+
+        // Verificar si la contraseña contiene al menos una letra mayúscula, una letra minúscula y un número
+        const hasUppercase = /[A-Z]/.test(password);
+        const hasLowercase = /[a-z]/.test(password);
+        const hasNumber = /\d/.test(password);
+
+        if (!hasUppercase || !hasLowercase || !hasNumber) {
+            return res.send('Password must contain at least one uppercase letter, one lowercase letter, and one number');
+        }
+        // se hashea la contraseña recibida en la solicitud
+        const newPassword = bcrypt.hashSync(req.body.password, 8);
+
+        const newUser = await User.create(
+            {
+                name: req.body.name,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: newPassword,
+                dni: req.body.dni,
+                phoneNumber: req.body.phoneNumber,
+                gender: req.body.gender,
+                birthdate: req.body.birthdate,
+                specialityId: req.body.specialityId,
+                collegiateNumber: req.body.collegiateNumber,
+                roleId: 2
+            }
+        );
+
+        return res.send(newUser);
+    } catch (error) {
+        return res.send('Something went wrong creating users ' + error.message)
+    }
+}
 
 //Obtencion del perfil de cada usuario
 userController.getUserProfile = async (req, res) => {
--- views/userRoutes.js
+++ views/userRoutes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
+// const isSuperAdmin = require('../middlewares/isSuperAdmin');
+const isAdmin = require('../middlewares/isAdmin');
 const isStaff = require('../middlewares/isStaff');
 const auth = require('../middlewares/verifyToken');
 
 // router.get('/', auth, isSuperAdmin, userController.getAllUsers)
+//registro de doctores
+router.post('/register', isAdmin, userController.register);
+//obtencion de los perfiles
 router.get('/profile', auth, userController.getUserProfile)
-router.get('/allProfiles', auth, isStaff, userController.getAllUsersProfile)
+router.get('/allProfiles', isStaff, isAdmin, userController.getAllUsersProfile)
 // update de los perfiles de usuario
 router.put('/updateProfile', auth, userController.updateProfile)
 
