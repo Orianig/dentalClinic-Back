8e4227c1248867277e2a4f0818e9ccd62c552f01
orianig <oriana.ig016@gmail.com>
2023-06-12 12:01:27 +0200

    feat: add dotenv- change the password - add enviroment variables

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+
 // DECLARO LAS CONST
 //direccionamiento 
 const express = require('express');
 //importa e contenido de db.js
 const db = require('./db');
 const router = require('./router');
+require('dotenv').config();
 //configuracion de la aplicación express
 const app = express();
 const PORT = 3000;
@@ -13,6 +15,7 @@ const PORT = 3000;
 app.use(express.json());
 //para gestionar todas las rutas del router
 app.use(router);
+// app.use(dotenv);
 //ruta general para mensaje de comprobacion de puerto
 app.get('/runningPort', (req, res) => {
     return res.send('Welcome to the beginning of nothingness');
